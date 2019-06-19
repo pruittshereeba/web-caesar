6eeed07d3419e0dda285a22aab473f71868c8618
Shereeba Pruitt <pruitt.shereeba@gmail.com>
2019-06-19 18:55:24 -0500

    add form

--- main.py
+++ main.py
@@ -8,3 +8,38 @@ def index():
     return "Hello World"
 
 app.run()
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form{
+                background-color: #eeee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+    <form method='POST'>
+        <label>Rotate by:</label>
+        <input type="text" name="rot" value="0" />
+        <textarea name="text">{0}</textarea>
+        input type="submit" value="Submit Query" />
+    </form>
+
+    </body>
+</html>
+""""
+@app.rout("/")
+def index():
+    return form.format('')

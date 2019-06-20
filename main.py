799d56e918204b572d2116f3df366282dd479b43
Shereeba Pruitt <pruitt.shereeba@gmail.com>
2019-06-19 19:57:36 -0500

    added encrypt to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -40,3 +41,11 @@ form = """
 @app.route("/")
 def index():
     return form.format('')
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rots = int(request.form['rot'])
+    msg = request.form['text']
+    return form.format(rotate_string(msg, rots))
+
+app.run()
